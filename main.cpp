b7e4f2704e1273b1a58972311b9c3cce79b6ea44
jcbalunis <jbalunis@mail.csuchico.edu>
2018-04-09 17:42:42 -0700

    Allllllllll DONE

--- main.cpp
+++ main.cpp
@@ -10,26 +10,57 @@ const int MIN_TEAMS = 1;
 
 int main()
 {
-  int periods;
-  int teams;
+        int periods, row;
+        int teams, col;
+        int P_NUMBER = 1;
+        vector < vector <int> > score_Board;
 
-  cout<<"How many competitors? ";
-  cin>>teams;
-  cout<<"How many scoring periods? ";
-  cin>>periods;
+        cout<<"How many competitors? ";
+        cin>>teams;
+        cout<<"How many scoring periods? ";
+        cin>>periods;
 
-  if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
-      teams > MAX_TEAMS || periods > MAX_PERIODS )
-  {
-    cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
-    cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
-    return 0;
-  }
-  else
-  {
-   //make scoreboard and fill it with zeros
-  
-   //once created, display the scoreboard
-  }
-  return 0;
+        
+        
+        
+      
+        if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
+                        teams > MAX_TEAMS || periods > MAX_PERIODS )
+        {
+                cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
+                cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
+                return 0;
+        }
+        else
+        {
+               score_Board.resize(teams);
+               for( int row = 0; row < score_Board.size(); row ++)
+               {
+                    score_Board[row].resize(periods);
+               }
+               
+               for(int row = 0; row < score_Board.size(); row ++)
+                {
+                        for(int col = 0; col < score_Board[row].size(); col ++)
+                        {
+                                score_Board [row][col] = 0;
+                        }
+                }
+
+                for (int row = 0; row < score_Board.size(); row ++)
+                { 
+                        cout<< "Player : " << P_NUMBER<< " ";
+                        for(int col = 0; col < score_Board[row].size(); col ++)
+                        { 
+                                cout<< score_Board[row][col];
+                                cout<< "|";
+                        }
+                       P_NUMBER ++;
+                       cout<<endl;
+                }
+                //make scoreboard and fill it with zeros
+
+                //once created, display the scoreboard
+        }
+        return 0;
 }
